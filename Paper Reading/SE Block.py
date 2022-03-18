2fe5ab4670327346133cfdb410d6c9f1134af889
Ashim Mahara <katyansun@gmail.com>
2022-03-18 08:40:46 +0545

    Bug fixes for SEBlock

--- Paper Reading/SE Block.py
+++ Paper Reading/SE Block.py
@@ -5,21 +5,31 @@ import torch.nn as nn
 class SEBlock(nn.Module):
     
     def __init__(self, input_channels: int, reduction_ratio: int = 16):
-                
+        
+        super(SEBlock, self).__init__()
+        
         self.bottleneck = input_channels / reduction_ratio
         
         self.excitation = nn.Sequential(
             nn.Linear(in_features = input_channels, 
-                      out_features = self.bottleneck),
+                      out_features = self.bottleneck,
+                      bias = False),
             nn.ReLU,
             nn.Linear(in_features = self.bottleneck,
-                      out_features = input_channels)
+                      out_features = input_channels,
+                      bias = False)
             )
         
     def forward(self, x):
         
+        ## Squeeze Module
         x1 = F.adaptive_avg_pool2d(x, 1)
-        x1 = self.excitation(x)
-        x1 = F.sigmoid(x)
         
-        return x * x1
+        ## Excitation Module
+        x1 = self.excitation(x)
+        
+        ## Scale Module
+        x1 = F.sigmoid(x)
+        x = x * x1
+        
+        return x
