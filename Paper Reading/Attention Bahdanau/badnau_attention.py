b41d969018b5efead159446643345dabfc5ca11c
Ashim Mahara <katyansun@gmail.com>
2022-03-27 18:02:46 +0545

    Added Various Notes; Added Badhnau Attention

---
+++ Paper Reading/Attention Bahdanau/badnau_attention.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BadhanauAttention(nn.Module):
+    
+    def __init__(self, hidden_dim):
+        self.alignment_nn = nn.Linear(in_features = hidden_dim * 2, out_features = hidden_dim)
+        self.v_t = nn.Linear(in_features = hidden_dim, out_features = 1)
+    
+    def forwad(self, encoder_hidden_states, decoder_hidden_state):
+        
+        ##score is the e_ij
+        
+        score = F.tanh(self.alignment_nn(torch.concat(encoder_hidden_states, decoder_hidden_state, dim = -1)))
+        score = self.v_t(score).squeeze(-1)
+        
+        context_vector = F.softmax(score, dim = -1)
+        return score, context_vector
+    
+
